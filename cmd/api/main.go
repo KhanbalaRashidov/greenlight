15c6d441b762bdd13b906af246e1787ac37c8d2f
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-08-03 10:36:26 +0400

    Added global logging and error handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"greenlight/internal/data"
-	"log"
+	"greenlight/internal/jsonlog"
 	"net/http"
 	"os"
 	"time"
@@ -37,7 +37,7 @@ type config struct {
 // logger, but it will grow to include a lot more as our build progresses.
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -53,16 +53,16 @@ func main() {
 
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
 
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -79,9 +79,12 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
